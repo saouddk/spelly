8c047b3d6f7124fce2921d8e6b2ce09824e64716
Saoud Khalifah <saouddk@users.noreply.github.com>
2021-01-02 22:16:37 -0500

    Update spelly.rb

--- lib/spelly.rb
+++ lib/spelly.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'hunspell-ffi'
+require 'ffi/hunspell'
 
 class Spelly
   attr_accessor :dict
@@ -8,13 +8,13 @@ class Spelly
   def initialize(language)
     path = File.expand_path('../lib/dict', File.dirname(__FILE__))
     # path = "#{Gem.loaded_specs['spelly'].full_gem_path}/lib/dict"
-    @dict = Hunspell.new("#{path}/#{language}.aff", "#{path}/#{language}.dic")
+    @dict = FFI::Hunspell::Dictionary.new("#{path}/#{language}.aff", "#{path}/#{language}.dic")
   end
 
   def spell_check(words)
     results = []
     words.each do |word|
-      results << { word: word, spell_check: @dict.spell(word) }
+      results << { word: word, spell_check: @dict.check?(word) }
     end
     results
   end
@@ -22,7 +22,7 @@ class Spelly
   def percentage_spelling_correct(words)
     spelling_correct = 0
     words.each do |word|
-      spelling_correct += 1 if @dict.spell(word)
+      spelling_correct += 1 if @dict.check?(word)
     end
     (spelling_correct.to_f/words.length.to_f)
   end
